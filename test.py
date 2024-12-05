2b553bd557eb442a057123172bb7ccfbc1a74145
ngoclamdhtn <92619159+ngoclamdhtn@users.noreply.github.com>
2024-12-05 08:39:54 +0700

    MAY BAN 8h30-05/12

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import D10_C1,D10_C2, D10_C3, D10_C4, D10_C5, D10_C6, D10_C7,D10_C8,D10_C9,D10_C10, D11_C1, D11_C2, D11_C3, D11_C4, D11_C5, D11_C6,D11_C7, D11_C8,D11_C9, D12_C1,D12_C2, D12_C4,D12_C5, D12_C7
 import D12_C3
+import ks10
 import my_module
 import math
 import random
@@ -23,26 +24,26 @@ import pyperclip
 
 
 #Test đúng sai
-# test, test_latex="",""
-# for i in range(1):
-# 	debai,debai_latex,loigiai_word,dap_an =D10_C5.y7y7u_L10_C5_B3_05()
-# 	#print(debai)
-# 	#print(loigiai_word)
-# 	test=test + f"{debai}\n{loigiai_word}\n"
-# 	test_latex=test_latex + f"{debai_latex}\n"
-# 	print(test_latex)
-# pyperclip.copy(test_latex)
-
-#Test tự luận
 test, test_latex="",""
 for i in range(1):
-	debai_word,loigiai_word,latex_tuluan,dap_an = D10_C3.npl_mk_L10_C3_B2_15()
+	debai,debai_latex,loigiai_word,dap_an =ks10.D10_CX_B0_14()
 	#print(debai)
 	#print(loigiai_word)
-	test=test + f"{debai_word}\n{loigiai_word}\n"
-	test_latex=test_latex + f"{latex_tuluan}\n"
+	test=test + f"{debai}\n{loigiai_word}\n"
+	test_latex=test_latex + f"{debai_latex}\n"
 	print(test_latex)
 pyperclip.copy(test_latex)
+
+#Test tự luận
+# test, test_latex="",""
+# for i in range(1):
+# 	debai_word,loigiai_word,latex_tuluan,dap_an = D10_C3.npl_mk_L10_C3_B2_15()
+# 	#print(debai)
+# 	#print(loigiai_word)
+# 	test=test + f"{debai_word}\n{loigiai_word}\n"
+# 	test_latex=test_latex + f"{latex_tuluan}\n"
+# 	print(test_latex)
+# pyperclip.copy(test_latex)
 
 
 
